d58fa649701885ac31ae3895c63b93b757b301e8
HenrikHeggland <hfh96@live.no>
2024-01-27 22:41:22 +0100

    Remove typo

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div>
       <h1>Twitch Chat Analyser</h1>
-      <button onClick={handleLogin}>Login with Twitch</button>;
+      <button onClick={handleLogin}>Login with Twitch</button>
       <TwitchChat />
     </div>
   );
